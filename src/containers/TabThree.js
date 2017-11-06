baa4cc6d32a23af5e8bbacfb44fc309d19a4a0f9
Harley McKee <harley@revelry.co>
2017-11-06 17:02:07 -0600

    Revert "Revert "F very basic react router usage + redux""

---
+++ src/containers/TabThree.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// TODO: rename
+class TabThree extends Component {
+	render() {
+		return (
+			<div>
+				this is tab three
+			</div>
+		)
+		
+	}
+}
+
+export default TabThree;
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+const initialState = {}
+
+export default function update(state = initialState, action) {
+  return state
+}
