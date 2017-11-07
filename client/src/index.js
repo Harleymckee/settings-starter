3409d9ec84404b80598fbac2144f294354f15db4
Harley McKee <h@harley.website>
2017-11-07 12:29:23 -0600

    container magic

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+// TODO get rid of this and just ignore oneline with global
 /* eslint-disable */
 
 import React from 'react';
@@ -8,6 +9,9 @@ import { createStore, combineReducers } from 'redux'
 import * as reducers from './reducers'
 import Layout from './components/Layout'
 import Home from './containers/Home'
+import TabOne from './containers/TabOne'
+import TabTwo from './containers/TabTwo'
+import TabThree from './containers/TabThree'
 
 // TODO: clean this up
 import './index.css';
@@ -23,17 +27,23 @@ const store = createStore(
   }),
 )
 
-const _path = path;
+// TODO: this is weird
+let _path;
+if (typeof path !== 'undefined') {
+	_path = path;
+} else {
+	_path = '/'
+}
 
 const content = {
-		'/': 'Home',
-		'/tabone': 'TabOne',
-		'/tabtwo': 'TabTwo',
-		'/tabthree': 'TabThree',
+		'/': Home,
+		'/tabone': TabOne,
+		'/tabtwo': TabTwo,
+		'/tabthree': TabThree,
 }
 
 const Content = content[_path];
-debugger
+
 ReactDOM.render(
 	<Provider store={store}>
 		<Layout>
